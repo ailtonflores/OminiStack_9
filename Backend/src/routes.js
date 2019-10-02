a816f485b23c62bd6298eeaf977ae7271043da84
ailton flores <ailtonflores20@gmail.com>
2019-10-02 16:29:22 -0300

    Ending Backend

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,14 +1,14 @@
 const express = require('express');
-//const multer = require('multer');
-//const uploadConfig = require('./config/upload');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
 
-//const SessionController = require ('./controllers/SessionController');
-//const SpotController = require ('./controllers/SpotController');
-//const DashboardController = require ('./controllers/DashboardController');
-//const BookingController = require ('./controllers/BookingController');
+const SessionController = require ('./controllers/SessionController');
+const SpotController = require ('./controllers/SpotController');
+const DashboardController = require ('./controllers/DashboardController');
+const BookingController = require ('./controllers/BookingController');
 
 const routes = express.Router();
-//const upload = multer(uploadConfig);
+const upload = multer(uploadConfig);
 
 // GET, POST, PUT, DELETE
 
